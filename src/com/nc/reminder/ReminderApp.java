bac712ab6b67e41a6b29a21322771591a6f35e3d
makensi <makensi@gmail.com>
2010-09-26 23:43:48 +0200

    first commit

---
+++ src/com/nc/reminder/ReminderApp.java
@@ -0,0 +1,17 @@
+package com.nc.reminder;
+
+import android.app.Application;
+
+/**
+ * Clase de aplicación para Reminder app.
+ * 
+ * @author makensi
+ * 
+ */
+public class ReminderApp extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
